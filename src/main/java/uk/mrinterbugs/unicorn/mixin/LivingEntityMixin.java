be083aa420d39069db0d08ecbc8a18ab7950d61f
Aedan Lawrence <aedan@mrinterbugs.uk>
2026-01-25 14:49:04 +0100

    Minor changes to reabilty in entitymixin

--- src/main/java/uk/mrinterbugs/unicorn/mixin/LivingEntityMixin.java
+++ src/main/java/uk/mrinterbugs/unicorn/mixin/LivingEntityMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,35 +20,33 @@ import uk.mrinterbugs.unicorn.UnicornHornHolder;
  */
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-    @Shadow public abstract void setHealth(float health);
-    @Shadow public abstract boolean clearStatusEffects();
-    @Shadow public abstract boolean addStatusEffect(StatusEffectInstance effect);
 
     /**
      * Consumes a slotted unicorn horn as a totem when a horse would die.
      */
     @Inject(method = "tryUseTotem", at = @At("HEAD"), cancellable = true)
     private void unicorn$tryHornTotem(DamageSource source, CallbackInfoReturnable<Boolean> cir) {
-        LivingEntity self = (LivingEntity) (Object) this;
-        if (!(self instanceof AbstractHorseEntity horse)) {
-            return;
-        }
-        if (source.isOf(DamageTypes.OUT_OF_WORLD) || !(horse instanceof UnicornHornHolder hornHolder)) {
-            return;
-        }
+        if (source.isOf(DamageTypes.OUT_OF_WORLD)) return;
 
-        ItemStack hornStack = hornHolder.unicorn$getHornStack();
-        if (hornStack.isEmpty() || !hornStack.isOf(UnicornMod.UNICORN_HORN)) {
-            return;
-        }
+        LivingEntity entity = (LivingEntity) (Object) this;
 
-        hornHolder.unicorn$setHornStack(ItemStack.EMPTY);
-        self.setHealth(1.0F);
-        self.clearStatusEffects();
-        self.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 900, 1));
-        self.addStatusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 100, 1));
-        self.addStatusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 800, 0));
-        self.getWorld().sendEntityStatus(self, EntityStatuses.USE_TOTEM_OF_UNDYING);
-        cir.setReturnValue(true);
+        if (entity instanceof AbstractHorseEntity horse && horse instanceof UnicornHornHolder hornHolder) {
+            ItemStack hornStack = hornHolder.unicorn$getHornStack();
+
+            if (!hornStack.isEmpty() && hornStack.isOf(UnicornMod.UNICORN_HORN)) {
+                hornStack.decrement(1); 
+                
+                entity.setHealth(1.0F);
+                entity.clearStatusEffects();
+                
+                entity.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 900, 1));
+                entity.addStatusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 100, 1));
+                entity.addStatusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 800, 0));
+
+                entity.getWorld().sendEntityStatus(entity, EntityStatuses.USE_TOTEM_OF_UNDYING);
+                
+                cir.setReturnValue(true);
+            }
+        }
     }
 }
